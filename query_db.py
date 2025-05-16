e4697c8c0f18d75da0eca73f620ad5f171fe3e86
raijinstorm <raijinstorm.myth@gmail.com>
2025-05-16 10:48:10 +0200

    Added support for loading into XML

--- query_db.py
+++ query_db.py
@@ -2,7 +2,7 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 import json
-
+from lxml import etree
 
 def extract(cur, query):
     cur.execute(query)
@@ -10,9 +10,28 @@ def extract(cur, query):
     rows = cur.fetchall()
     return [dict(zip(cols, row)) for row in rows]
 
-def load_to_json(data: dict, filepath: str):
+def load_to_json(data, filename):
+    filepath = f"output/{filename}.json"
     with open(filepath, "w") as f:
         json.dump(data, f, indent=2, default=str)
+
+def load_to_xml(data, filename, record_tag):
+    root = etree.Element(f"_{filename}")
+    
+    for item in data:
+        record = etree.SubElement(root, record_tag) 
+        for key, value in item.items():
+            child = etree.SubElement(record, key)
+            child.text = str(value)
+    
+    tree = etree.ElementTree(root)
+    filepath=f"output/{filename}.xml"
+    tree.write(
+        filepath,
+        pretty_print=True,
+        xml_declaration=True,
+        encoding="UTF-8"
+    )
 
 def get_queries(folder_path):
     queries = {}
@@ -41,15 +60,19 @@ def main():
 
     os.makedirs("output", exist_ok=True)
     
+    is_json = False
+    
     for name, sql in queries.items():
         data = extract(cur, sql)
-        filename = f"output/{name}.json"
-        load_to_json(data, filename)
-        print(f"Loaded data to {filename}")
+        if is_json:
+            load_to_json(data, name)
+            print(f"Loaded data to output/{name}.json")
+        else:
+            load_to_xml(data, name, "record_r")
+            print(f"Loaded data to output/{name}.xml")
     
     cur.close()
     conn.close()
-    
     
 if __name__ == "__main__":
     main()
